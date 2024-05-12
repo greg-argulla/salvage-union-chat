69c7a2445260e985d4e36b27c8b060de9c34d5db
greg-argulla <139342116+gregargulla-i3soft@users.noreply.github.com>
2024-05-12 15:54:33 +0800

    sad

--- src/App.jsx
+++ src/App.jsx
@@ -464,10 +464,6 @@ const getImage = (str) => {
   return str.substring(str.indexOf("<") + 1, str.lastIndexOf(">"));
 };
 
-const getSFX = (str) => {
-  return str.substring(str.indexOf("$") + 1, str.lastIndexOf("$"));
-};
-
 function App() {
   const [text, setText] = useState("");
   const [isOBRReady, setIsOBRReady] = useState(false);
@@ -489,23 +485,10 @@ function App() {
   const ChatInstance = (props) => {
     let propsString = JSON.stringify(props);
     const imageURL = getImage(propsString);
-    const sfxURL = getSFX(propsString);
 
     if (imageURL) {
       propsString = propsString.replace("<" + imageURL + ">", "");
     }
-
-    if (sfxURL && !props.noSFX) {
-      propsString = propsString.replace("$" + sfxURL + "$", " ♫ ");
-    }
-
-    useEffect(() => {
-      if (index > props.chatLength - 1) {
-        const audio = new Audio(sfxURL);
-        audio.volume = 0.2;
-        audio.play();
-      }
-    }, []);
 
     const { item, index } = JSON.parse(propsString);
 
